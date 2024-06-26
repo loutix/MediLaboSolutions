4f9b46cf126ffba996f254739e7af329f0134852
loutix <62959342+loutix@users.noreply.github.com>
2024-06-26 17:08:40 +0200

    add and configure docker-compose.yml

--- assessment-service/src/main/java/com/ocr/assessment_service/proxies/NoteProxy.java
+++ assessment-service/src/main/java/com/ocr/assessment_service/proxies/NoteProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "note-service", url = "localhost:8090", path = "note-service")
+@FeignClient(name = "note-service", path = "note-service")
 public interface NoteProxy {
     @GetMapping("/patient/{id}")
     List<Note> getNoteByPatientId(@PathVariable("id") Integer id);
